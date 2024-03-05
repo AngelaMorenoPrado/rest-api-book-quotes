3ab7c8a1f5bbe122004988ef4afc0f48426d70dc
AngelaMorenoPrado <angelamorenoprado@gmail.com>
2024-03-05 15:11:53 +0100

    feat: return only book_title, quote, and author fields

--- services/book_quotes.py
+++ services/book_quotes.py
@@ -6,7 +6,7 @@ from bson.json_util import dumps
 
 def get_book_quotes():
     try:
-        return dumps(db.find())
+        return dumps(db.find({}, {"book_title": 1, "quote": 1, "author": 1, "_id": 0}))
     except Exception as e:
         return make_response({'message': str(e)}, 404)
     
