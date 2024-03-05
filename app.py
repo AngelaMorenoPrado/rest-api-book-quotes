c3d10bf4ce3077a5f88ff47da92642c9d3dec42f
AngelaMorenoPrado <angelamorenoprado@gmail.com>
2024-03-05 15:07:22 +0100

    chore: change to MongoClient

--- app.py
+++ app.py
@@ -1,13 +1,10 @@
 import os
 from flask import Flask
 from api.book_quotes import book_quotes
-from mongoengine import connect
 
 app = Flask(__name__)
 
 app.register_blueprint(book_quotes)
-
-connect(host=os.environ.get('MONGO_URI'))
 
 if __name__ == "__main__":
     app.debug = True
