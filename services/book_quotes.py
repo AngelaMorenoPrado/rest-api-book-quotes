49049496f1dccf32030eeea969496b3c30d0697c
AngelaMorenoPrado <angelamorenoprado@gmail.com>
2024-03-05 16:58:49 +0100

    chore: remove not used imports

--- services/book_quotes.py
+++ services/book_quotes.py
@@ -1,5 +1,5 @@
 from json import dumps
-from flask import make_response, jsonify
+from flask import make_response
 from model.book_quotes import Quotes
 from database.database import db
 from bson.json_util import dumps
@@ -11,7 +11,7 @@ def get_book_quotes():
             {"$sample": {"size": 25}},
             {"$project": {"book_title": 1, "quote": 1, "author": 1, "_id": 0}}
         ]
-        
+
         return dumps(db.aggregate(pipeline))
     except Exception as e:
         return make_response({'message': str(e)}, 404)
