d9a52a39c6e5fcda1b6b9e24439c0c23142e7741
AngelaMorenoPrado <angelamorenoprado@gmail.com>
2024-03-05 12:50:16 +0100

    chore: move add_book_quotes from api to services

--- api/book_quotes.py
+++ api/book_quotes.py
@@ -1,5 +1,5 @@
 from flask import Blueprint, request, make_response
-from services.book_quotes import get_book_quotes
+from services.book_quotes import get_book_quotes, add_book_quote
 from model.book_quotes import Quotes
 book_quotes = Blueprint('book_quotes', __name__)
 
@@ -10,16 +10,4 @@ def get_quotes():
 
 @book_quotes.route("/book-quotes", methods=["POST"])
 def add_quote():
-    try:
-        quote_data = request.json
-
-        book_title = quote_data.get('book_title')
-        quote = quote_data.get('quote')
-        author = quote_data.get('author')
-
-        quotes = Quotes(book_title=book_title, quote=quote, author=author)
-
-        quotes.save()
-        return make_response({'message': 'Successfully saved'}, 201)
-    except Exception as e:
-        return make_response({'message': str(e)}, 404)
+    return add_book_quote(request.json)
--- services/book_quotes.py
+++ services/book_quotes.py
@@ -11,3 +11,16 @@ def get_book_quotes():
         return {"quotes": "HOLA"}
     except Exception as e:
         return make_response({'message': str(e)}, 404)
+    
+def add_book_quote(quote_data):
+    try:
+        book_title = quote_data.get('book_title')
+        quote = quote_data.get('quote')
+        author = quote_data.get('author')
+
+        quotes = Quotes(book_title=book_title, quote=quote, author=author)
+
+        quotes.save()
+        return make_response({'message': 'Successfully saved'}, 201)
+    except Exception as e:
+        return make_response({"message": str(e)}, 404)
